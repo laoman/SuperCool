28ce529cef9731ec77c938682aa64faa393ff7ad
laoman <indefixx@gmail.com>
2016-01-14 14:00:54 +0200

    testing git on xcode

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg:UIImageView!
+    @IBOutlet weak var unCoolBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        coolLogo.hidden = true
+        coolBg.hidden = true
+        unCoolBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func unCoolBtn(sender: AnyObject) {
+        if coolLogo.hidden == true {
+            coolLogo.hidden = false
+            coolBg.hidden = false
+            unCoolBtn.setTitle("f*** awesomeness!", forState: UIControlState.Normal)
+            unCoolBtn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        }else{
+            coolLogo.hidden = true
+            coolBg.hidden = true
+            unCoolBtn.setTitle("Let's be Awesome!", forState: UIControlState.Normal)
+            unCoolBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        }
+    }
 }
 
